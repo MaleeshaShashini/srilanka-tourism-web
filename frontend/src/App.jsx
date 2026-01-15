f73a4d4d4a9d66a5ad49e087005c6c1ef8dcd7a1
MaleeshaShashini <maleeshashashini70@gmail.com>
2026-01-15 16:03:51 +0530

    Add Static Data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,39 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+
+   const mockDestinations = [
+       {
+         id: 1,
+         name: "Sigiriya Lion Rock",
+         location: "Matale",
+         description: "Ancient rock fortress and palace built by King Kashyapa.",
+         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Sigiriya_Rock.jpg"
+       },
+       {
+         id: 2,
+         name: "Ella Nine Arch Bridge",
+         location: "Badulla",
+         description: "A colonial-era railway bridge famous for its scenic beauty.",
+         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/bb/Nine_Arch_Bridge_Ella.jpg"
+       },
+       {
+         id: 3,
+         name: "Mirissa Beach",
+         location: "Matara",
+         description: "Famous for whale watching and surfing.",
+         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/f4/Mirissa_Beach.jpg"
+       }
+     ];
+
   const [destinations, setDestinations] = useState([])
 
-  useEffect(() => {
-      fetch('http://localhost:8080/destinations')
-        .then(response => response.json())
-        .then(data => setDestinations(data))
-        .catch(error => console.error('Error fetching data:', error));
-    }, [])
+//   useEffect(() => {
+//       fetch('http://localhost:8080/destinations')
+//         .then(response => response.json())
+//         .then(data => setDestinations(data))
+//         .catch(error => console.error('Error fetching data:', error));
+//     }, [])
 
   return (
    <div className="container">
