eaa9f1e4778a54cee952879d0d0167021acbf4a2
MaleeshaShashini <maleeshashashini70@gmail.com>
2026-01-20 12:21:05 +0530

    create destination card components

---
+++ frontend/src/component/DestinationCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function DestinationCard({ image, title, description }){
+    return(
+        <div className="card">
+
+          <div className="overflow-hidden h-100 shadow-sm border-0 overflow-hidden destination-card">
+                  <img
+                    src={image}
+                    className="card-img-top destination-img"
+                    alt={title}
+                  />
+          </div>
+
+          <div className="card-body">
+            <h5 className="card-title fw-bold">{title}</h5>
+            <p className="card-text text-muted"> {description}</p>
+            <a href="#" className="btn btn-primary btn-outline-success rounded-pill btn-sm">View Details</a>
+          </div>
+        </div>
+
+
+        );
+
+    }
+
+export default DestinationCard;
