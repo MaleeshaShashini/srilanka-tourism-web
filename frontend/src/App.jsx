940bce69c094c8be2524e77ccce4977b1e794e72
MaleeshaShashini <maleeshashashini70@gmail.com>
2026-01-20 13:24:42 +0530

    Feat: Define destination categories and data structure
    #Added data array for main categories
    #Prepared descriptions and structure for mapping into cards.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,30 +5,6 @@ import Hero from './component/Hero'
 
 function App() {
 
-   const mockDestinations = [
-       {
-         id: 1,
-         name: "Sigiriya Lion Rock",
-         location: "Matale",
-         description: "Ancient rock fortress and palace built by King Kashyapa.",
-         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Sigiriya_Rock.jpg"
-       },
-       {
-         id: 2,
-         name: "Ella Nine Arch Bridge",
-         location: "Badulla",
-         description: "A colonial-era railway bridge famous for its scenic beauty.",
-         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/bb/Nine_Arch_Bridge_Ella.jpg"
-       },
-       {
-         id: 3,
-         name: "Mirissa Beach",
-         location: "Matara",
-         description: "Famous for whale watching and surfing.",
-         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/f4/Mirissa_Beach.jpg"
-       }
-     ];
-
   const [destinations, setDestinations] = useState()
 
 //   useEffect(() => {
---
+++ frontend/src/component/Destinations.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import DestinationCard from './DestinationCard';
+
+function Destinations(){
+    const destinations = [
+           {
+             image: "/img/beach.jpg",
+             title:"Pristine Beaches",
+             description:"Relax on golden sands and dive into the crystal clear blue waters."
+           },
+           {
+              image: "/img/safari.jpg",
+              title:"Wildlife Safaris",
+              description:"Witness majestic elephants and leopards in their natural habitat."
+           },
+           {
+               image: "/img/mountain.jpg",
+               title:"Misty Mountains",
+               description:"Escape to the cool highlands filled with lush tea plantations."
+           },
+
+           {
+               image: "/img/rainforests.jpg",
+               title:"Rainforests & Eco",
+               description:"Trek through dense rainforests rich in biodiversity and nature."
+           },
+
+           {
+               image: "/img/antiant.jpg",
+               title:"Ancient Heritage",
+               description:"Explore the majestic ruins and temples of ancient kingdoms."
+           },
+
+           {
+               image: "/img/waterfall.webp",
+               title:"Scenic Waterfall",
+               description:"Discover the breathtaking beauty of cascading waterfalls."
+           },
+
+           {
+                image: "/img/adventure.jpg",
+                title:"Adventure & Thrills",
+                description:"Experience surfing, hiking, and white-water rafting adventures."
+           },
+
+           {
+                image: "/img/ayurvedic.jpg",
+                title:"Wellness & Ayurveda",
+                description:"Rejuvenate your mind and body with traditional healing therapies."
+           },
+           {
+                image: "/img/colombo.jpg",
+                title:"City Life & Shopping",
+                description:"Experience the vibrant culture, shopping, and nightlife of Colombo."
+           }
+
+         ];
+    }
+export default Destinations;
